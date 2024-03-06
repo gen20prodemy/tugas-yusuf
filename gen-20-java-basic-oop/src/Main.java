7c3bdb1f1190e9d5d5e305ef437cc3def6aed86a
yusufva <yusufvalent@gmail.com>
2024-03-06 20:28:31 +0700

    feature: implementing product and category object

--- gen-20-java-basic-oop/src/Main.java
+++ gen-20-java-basic-oop/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FoodCategory foodCategory = new FoodCategory();
+        BeverageCategory beverageCategory = new BeverageCategory();
+        MedicineCategory medicineCategory = new MedicineCategory();
+
+        FoodProduct miInstan = new FoodProduct("Mi Instan", 3500);
+        FoodProduct kue = new FoodProduct("Kue", 5000);
+        BeverageProduct airMineral = new BeverageProduct("Air Mineral", 3000);
+        BeverageProduct tehHijau = new BeverageProduct("Teh Hijau", 7000);
+        MedicineProduct obatPusing = new MedicineProduct("Obat Pusing", 12000);
+        MedicineProduct obatMual = new MedicineProduct("Obat Mual", 8500);
+
+        System.out.println(foodCategory.getCategoryDescription());
+        miInstan.displayProductInfo();
+        System.out.println();
+        kue.displayProductInfo();
+        System.out.println();
+
+        System.out.println(beverageCategory.getCategoryDescription());
+        airMineral.displayProductInfo();
+        System.out.println();
+        tehHijau.displayProductInfo();
+        System.out.println();
+
+        System.out.println(medicineCategory.getCategoryDescription());
+        obatPusing.displayProductInfo();
+        System.out.println();
+        obatMual.displayProductInfo();
     }
 }
