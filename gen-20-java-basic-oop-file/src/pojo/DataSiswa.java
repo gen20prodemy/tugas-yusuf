7ed537763258b44f60c093bfba2c5c50172c26ed
yusufva <yusufvalent@gmail.com>
2024-03-08 15:32:47 +0700

    feature: creating DataSiswa pojo

---
+++ gen-20-java-basic-oop-file/src/pojo/DataSiswa.java
@@ -0,0 +1,31 @@
+package pojo;
+
+public class DataSiswa {
+    private int id;
+    private String nama;
+    private int kelas;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(int kelas) {
+        this.kelas = kelas;
+    }
+}
