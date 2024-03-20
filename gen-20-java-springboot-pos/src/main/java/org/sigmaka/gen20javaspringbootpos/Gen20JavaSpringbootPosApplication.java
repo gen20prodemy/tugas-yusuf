23430fd78865c2dc30dcac19cecb90638228b0db
yusufva <yusufvalent@gmail.com>
2024-03-20 11:23:08 +0700

    initial commit

---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/Gen20JavaSpringbootPosApplication.java
@@ -0,0 +1,13 @@
+package org.sigmaka.gen20javaspringbootpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen20JavaSpringbootPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gen20JavaSpringbootPosApplication.class, args);
+    }
+
+}
---
+++ gen-20-java-springboot-pos/src/test/java/org/sigmaka/gen20javaspringbootpos/Gen20JavaSpringbootPosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sigmaka.gen20javaspringbootpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen20JavaSpringbootPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
