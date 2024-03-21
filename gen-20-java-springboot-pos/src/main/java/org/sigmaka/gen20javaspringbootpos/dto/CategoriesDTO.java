c33d89cd1b1af5b2d11dabc7df0ffe1bfa66fd48
yusufva <yusufvalent@gmail.com>
2024-03-21 08:59:04 +0700

    feature: Creating DTO

---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/dto/CategoriesDTO.java
@@ -0,0 +1,25 @@
+package org.sigmaka.gen20javaspringbootpos.dto;
+
+import org.sigmaka.gen20javaspringbootpos.entity.CategoriesEntity;
+
+import java.sql.Timestamp;
+
+public class CategoriesDTO {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CategoriesEntity dtoToEntity(){
+        CategoriesEntity category = new CategoriesEntity();
+        category.setName(name);
+        category.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        category.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+        return category;
+    }
+}
---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/dto/ProductsDTO.java
@@ -0,0 +1,56 @@
+package org.sigmaka.gen20javaspringbootpos.dto;
+
+import org.sigmaka.gen20javaspringbootpos.entity.ProductsEntity;
+
+import java.sql.Timestamp;
+
+public class ProductsDTO {
+    private String name;
+    private int price;
+    private int categoryId;
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public ProductsEntity dtoToEntity(){
+        ProductsEntity product = new ProductsEntity();
+
+        product.setName(name);
+        product.setPrice(price);
+        product.setCategoryId(categoryId);
+        product.setQuantity(quantity);
+        product.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        product.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+        return product;
+    }
+}
