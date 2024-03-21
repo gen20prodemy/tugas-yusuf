53e1078c46a5028db8987744a178d5a84dcbacee
yusufva <yusufvalent@gmail.com>
2024-03-21 08:59:38 +0700

    feature: Creating Controller

---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/controller/CategoriesController.java
@@ -0,0 +1,42 @@
+package org.sigmaka.gen20javaspringbootpos.controller;
+
+import org.sigmaka.gen20javaspringbootpos.dto.CategoriesDTO;
+import org.sigmaka.gen20javaspringbootpos.entity.CategoriesEntity;
+import org.sigmaka.gen20javaspringbootpos.helper.GlobalHttpResponse;
+import org.sigmaka.gen20javaspringbootpos.service.CategoriesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/category")
+public class CategoriesController {
+    private final CategoriesService categoriesService;
+
+    @Autowired
+    public CategoriesController(CategoriesService categoriesService) {
+        this.categoriesService = categoriesService;
+    }
+
+    @GetMapping("/")
+    public GlobalHttpResponse<List<CategoriesEntity>> getAll(){
+        return categoriesService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public GlobalHttpResponse<Optional<CategoriesEntity>> getById(@PathVariable("id") int id){
+        return categoriesService.getById(id);
+    }
+
+    @PostMapping("/")
+    public GlobalHttpResponse<CategoriesEntity> insert(@RequestBody CategoriesDTO categoriesDTO){
+        return categoriesService.insert(categoriesDTO);
+    }
+
+    @PutMapping("/{id}")
+    public GlobalHttpResponse<CategoriesEntity> updateById(@PathVariable("id") int id, @RequestBody CategoriesDTO categoriesDTO){
+        return categoriesService.updateById(id, categoriesDTO);
+    }
+}
---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/controller/ProductsController.java
@@ -0,0 +1,42 @@
+package org.sigmaka.gen20javaspringbootpos.controller;
+
+import org.sigmaka.gen20javaspringbootpos.dto.ProductsDTO;
+import org.sigmaka.gen20javaspringbootpos.entity.ProductsEntity;
+import org.sigmaka.gen20javaspringbootpos.helper.GlobalHttpResponse;
+import org.sigmaka.gen20javaspringbootpos.service.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/products")
+public class ProductsController {
+    private final ProductsService productsService;
+
+    @Autowired
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+    @GetMapping("/")
+    public GlobalHttpResponse<List<ProductsEntity>> getAll(){
+        return productsService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public GlobalHttpResponse<Optional<ProductsEntity>> getById(@PathVariable("id") int id){
+        return productsService.getById(id);
+    }
+
+    @PostMapping("/")
+    public GlobalHttpResponse<ProductsEntity> insert(@RequestBody ProductsDTO productsDTO){
+        return productsService.insert(productsDTO);
+    }
+
+    @PutMapping("/{id}")
+    public GlobalHttpResponse<ProductsEntity> updateById(@PathVariable("id") int id, @RequestBody ProductsDTO productsDTO){
+        return productsService.updateById(productsDTO, id);
+    }
+}
