d4253ffda7ea76034d814b6b5b7dd9265cbaf72d
yusufva <yusufvalent@gmail.com>
2024-03-21 08:59:28 +0700

    feature: Creating Services

---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/service/CategoriesService.java
@@ -0,0 +1,51 @@
+package org.sigmaka.gen20javaspringbootpos.service;
+
+import org.sigmaka.gen20javaspringbootpos.dto.CategoriesDTO;
+import org.sigmaka.gen20javaspringbootpos.entity.CategoriesEntity;
+import org.sigmaka.gen20javaspringbootpos.helper.GlobalHttpResponse;
+import org.sigmaka.gen20javaspringbootpos.repository.CategoriesRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoriesService {
+    private final CategoriesRepo categoriesRepo;
+
+    @Autowired
+    public CategoriesService(CategoriesRepo categoriesRepo) {
+        this.categoriesRepo = categoriesRepo;
+    }
+
+    public GlobalHttpResponse<List<CategoriesEntity>> getAll(){
+        return new GlobalHttpResponse<>(200, "Success retrieve data", categoriesRepo.findAll());
+    }
+
+    public GlobalHttpResponse<Optional<CategoriesEntity>> getById(int id){
+        Optional<CategoriesEntity> result = categoriesRepo.findById(id);
+        if (result.isEmpty()) {
+            return new GlobalHttpResponse<>(404, "Data not found", result);
+        }
+
+        return new GlobalHttpResponse<>(200, "Success retrieve data", result);
+    }
+
+    public GlobalHttpResponse<CategoriesEntity> insert(CategoriesDTO categoriesDTO){
+        return new GlobalHttpResponse<>(201, "Success insert data", categoriesRepo.save(categoriesDTO.dtoToEntity()));
+    }
+
+    public GlobalHttpResponse<CategoriesEntity> updateById(int id, CategoriesDTO categoriesDTO){
+        Optional<CategoriesEntity> optional = categoriesRepo.findById(id);
+        if (optional.isEmpty()) {
+            return new GlobalHttpResponse<>(404, "Data not found", new CategoriesEntity());
+        }
+
+        CategoriesEntity category = optional.get();
+        category.setName(categoriesDTO.getName());
+        category.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+        return new GlobalHttpResponse<>(200, "Success update data", categoriesRepo.save(category));
+    }
+}
---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/service/ProductsService.java
@@ -0,0 +1,52 @@
+package org.sigmaka.gen20javaspringbootpos.service;
+
+import org.sigmaka.gen20javaspringbootpos.dto.ProductsDTO;
+import org.sigmaka.gen20javaspringbootpos.entity.ProductsEntity;
+import org.sigmaka.gen20javaspringbootpos.helper.GlobalHttpResponse;
+import org.sigmaka.gen20javaspringbootpos.repository.ProductsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductsService {
+    private final ProductsRepo productsRepo;
+
+    @Autowired
+    public ProductsService(ProductsRepo productsRepo) {
+        this.productsRepo = productsRepo;
+    }
+
+    public GlobalHttpResponse<List<ProductsEntity>> getAll(){
+        List<ProductsEntity> result = productsRepo.findAll();
+        return new GlobalHttpResponse<>(200, "Success retrieve data", result);
+    }
+
+    public GlobalHttpResponse<Optional<ProductsEntity>> getById(int id){
+        Optional<ProductsEntity> result = productsRepo.findById(id);
+        return new GlobalHttpResponse<>(200,"Success retrieve data", result);
+    }
+
+    public GlobalHttpResponse<ProductsEntity> insert(ProductsDTO productsDTO){
+        ProductsEntity result = productsRepo.save(productsDTO.dtoToEntity());
+        return new GlobalHttpResponse<>(201, "Data has been created", result);
+    }
+    
+    public GlobalHttpResponse<ProductsEntity> updateById(ProductsDTO productsDTO, int id){
+        Optional<ProductsEntity> optional = productsRepo.findById(id);
+        if (optional.isEmpty()) {
+            return new GlobalHttpResponse<>(404, "Data not found", new ProductsEntity());
+        }
+        ProductsEntity product = optional.get();
+        product.setName(productsDTO.getName());
+        product.setPrice(productsDTO.getPrice());
+        product.setCategoryId(productsDTO.getCategoryId());
+        product.setQuantity(productsDTO.getQuantity());
+        product.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+        ProductsEntity result = productsRepo.save(product);
+        return new GlobalHttpResponse<>(200, "Success update data", result);
+    }
+}
