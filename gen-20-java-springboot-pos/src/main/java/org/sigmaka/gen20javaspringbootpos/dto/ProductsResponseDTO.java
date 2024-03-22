93025d0c04167d2abc37d18ded5f527800e24c10
yusufva <yusufvalent@gmail.com>
2024-03-22 09:35:01 +0700

    feature: adding ProductsResponseDTO

---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/dto/ProductsResponseDTO.java
@@ -0,0 +1,49 @@
+package org.sigmaka.gen20javaspringbootpos.dto;
+
+public class ProductsResponseDTO {
+    private int id;
+    private String name;
+    private int price;
+    private int quantity;
+    private CategoriesDTO category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public CategoriesDTO getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoriesDTO category) {
+        this.category = category;
+    }
+}
