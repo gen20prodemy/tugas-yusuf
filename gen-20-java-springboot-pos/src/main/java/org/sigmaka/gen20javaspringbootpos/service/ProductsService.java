3ec7164f2fa7395e7ad2c08fd00cbbdfd909385b
yusufva <yusufvalent@gmail.com>
2024-03-22 09:34:25 +0700

    refactor: refactoring and fixing changes on relation logic

--- gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/service/ProductsService.java
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/service/ProductsService.java
@@ -1,62 +1,88 @@
 package org.sigmaka.gen20javaspringbootpos.service;
 
 import org.sigmaka.gen20javaspringbootpos.dto.ProductsDTO;
+import org.sigmaka.gen20javaspringbootpos.dto.ProductsResponseDTO;
+import org.sigmaka.gen20javaspringbootpos.entity.CategoriesEntity;
 import org.sigmaka.gen20javaspringbootpos.entity.ProductsEntity;
 import org.sigmaka.gen20javaspringbootpos.helper.GlobalHttpResponse;
+import org.sigmaka.gen20javaspringbootpos.repository.CategoriesRepo;
 import org.sigmaka.gen20javaspringbootpos.repository.ProductsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ProductsService {
     private final ProductsRepo productsRepo;
+    private final CategoriesRepo categoriesRepo;
 
     @Autowired
-    public ProductsService(ProductsRepo productsRepo) {
+    public ProductsService(ProductsRepo productsRepo, CategoriesRepo categoriesRepo) {
         this.productsRepo = productsRepo;
+        this.categoriesRepo = categoriesRepo;
     }
 
-    public GlobalHttpResponse<List<ProductsEntity>> getAll(){
-        List<ProductsEntity> result = productsRepo.findAll();
+    public GlobalHttpResponse<List<ProductsResponseDTO>> getAll(){
+        List<ProductsEntity> products = productsRepo.findAll();
+        List<ProductsResponseDTO> result = new ArrayList<>();
+        for(ProductsEntity data : products){
+            result.add(data.entityToDto());
+        }
         return new GlobalHttpResponse<>(200, "Success retrieve data", result);
     }
 
-    public GlobalHttpResponse<Optional<ProductsEntity>> getById(int id){
+    public GlobalHttpResponse<ProductsResponseDTO> getById(int id){
         Optional<ProductsEntity> result = productsRepo.findById(id);
-        return new GlobalHttpResponse<>(200,"Success retrieve data", result);
+        if (result.isEmpty()) {
+            return new GlobalHttpResponse<>(404, "Data not found", result.get().entityToDto());
+        }
+        return new GlobalHttpResponse<>(200,"Success retrieve data", result.get().entityToDto());
     }
 
-    public GlobalHttpResponse<ProductsEntity> insert(ProductsDTO productsDTO){
-        ProductsEntity result = productsRepo.save(productsDTO.dtoToEntity());
-        return new GlobalHttpResponse<>(201, "Data has been created", result);
+    public GlobalHttpResponse<ProductsResponseDTO> insert(ProductsDTO productsDTO){
+        ProductsEntity product = productsDTO.dtoToEntity();
+        CategoriesEntity categories = categoriesRepo.findById(productsDTO.getCategoryId()).orElse(null);
+
+        if (categories == null) {
+            return new GlobalHttpResponse<>(404, "Category data not found", new ProductsEntity().entityToDto());
+        }
+
+        product.setCategory(categories);
+        ProductsEntity result = productsRepo.save(product);
+        return new GlobalHttpResponse<>(201, "Data has been created", result.entityToDto());
     }
     
-    public GlobalHttpResponse<ProductsEntity> updateById(ProductsDTO productsDTO, int id){
+    public GlobalHttpResponse<ProductsResponseDTO> updateById(ProductsDTO productsDTO, int id){
         Optional<ProductsEntity> optional = productsRepo.findById(id);
         if (optional.isEmpty()) {
-            return new GlobalHttpResponse<>(404, "Data not found", new ProductsEntity());
+            return new GlobalHttpResponse<>(404, "Data not found", new ProductsEntity().entityToDto());
         }
+        CategoriesEntity categories = categoriesRepo.findById(productsDTO.getCategoryId()).orElse(null);
+        if (categories == null) {
+            return new GlobalHttpResponse<>(404, "Category data not found", new ProductsEntity().entityToDto());
+        }
+
         ProductsEntity product = optional.get();
         product.setName(productsDTO.getName());
         product.setPrice(productsDTO.getPrice());
-        product.setCategoryId(productsDTO.getCategoryId());
+        product.setCategory(categories);
         product.setQuantity(productsDTO.getQuantity());
         product.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
         ProductsEntity result = productsRepo.save(product);
-        return new GlobalHttpResponse<>(200, "Success update data", result);
+        return new GlobalHttpResponse<>(200, "Success update data", result.entityToDto());
     }
 
-    public GlobalHttpResponse<ProductsEntity> deleteById(int id){
+    public GlobalHttpResponse<ProductsResponseDTO> deleteById(int id){
         Optional<ProductsEntity> optional = productsRepo.findById(id);
         if (optional.isEmpty()) {
-            return new GlobalHttpResponse<>(404, "Data not found", new ProductsEntity());
+            return new GlobalHttpResponse<>(404, "Data not found", new ProductsEntity().entityToDto());
         }
         ProductsEntity product = optional.get();
         productsRepo.deleteById(id);
-        return new GlobalHttpResponse<>(200, "Success delete data", product);
+        return new GlobalHttpResponse<>(200, "Success delete data", product.entityToDto());
     }
 }
