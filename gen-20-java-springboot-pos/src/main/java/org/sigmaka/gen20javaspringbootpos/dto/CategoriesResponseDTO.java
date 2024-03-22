462c82af2323048eb3273d135e452e28d5613992
yusufva <yusufvalent@gmail.com>
2024-03-22 09:37:22 +0700

    feature: creating CategoriesResponseDTO

---
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/dto/CategoriesResponseDTO.java
@@ -0,0 +1,34 @@
+package org.sigmaka.gen20javaspringbootpos.dto;
+
+import java.util.List;
+
+public class CategoriesResponseDTO {
+    private  int id;
+    private String name;
+    private List<ProductsResponseDTO> products;
+
+    public List<ProductsResponseDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductsResponseDTO> products) {
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
