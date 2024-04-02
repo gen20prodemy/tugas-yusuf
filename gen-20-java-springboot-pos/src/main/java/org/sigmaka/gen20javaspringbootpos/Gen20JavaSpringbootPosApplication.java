cf802348584b192cfbb6b99f28c5501d7d252d9d
yusufva <yusufvalent@gmail.com>
2024-04-02 13:53:00 +0700

    feature: adding restTemplate

--- gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/Gen20JavaSpringbootPosApplication.java
+++ gen-20-java-springboot-pos/src/main/java/org/sigmaka/gen20javaspringbootpos/Gen20JavaSpringbootPosApplication.java
@@ -2,12 +2,19 @@ package org.sigmaka.gen20javaspringbootpos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Gen20JavaSpringbootPosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Gen20JavaSpringbootPosApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 
 }
